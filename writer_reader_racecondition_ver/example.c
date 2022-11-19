84ec4865179791983cf10fe5a8098743cf02c8d4
Huejii <94972402+Huejii@users.noreply.github.com>
2022-11-19 21:50:17 +0900

    Update example.c

--- writer_reader_racecondition_ver/example.c
+++ writer_reader_racecondition_ver/example.c
@@ -6,11 +6,9 @@ char* S = "Happy Merry Christmas~! ";
 
 int main()
 {
-    char* new;
-    strncpy(new, S, strlen(S));
     for (int i = 0; i< strlen(S); i++) {
         if (S[i] >= 'a' && S[i] <= 'z')
-            new[i] = toupper(new[i]);
+            S[i] = toupper(S[i]);
     }
     printf("%s\n", S);
     return 0;
