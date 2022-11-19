408002f5861d7afae4a3d353e99e4efdabf5dc2f
Huejii <94972402+Huejii@users.noreply.github.com>
2022-11-19 17:11:27 +0900

    대대적인 수정

--- writer_reader_racecondition_ver/writers_readers_racecondition_ver.c
+++ writer_reader_racecondition_ver/writers_readers_racecondition_ver.c
@@ -22,12 +22,11 @@ char* S = "Happy Merry Christmas~!";
 int count = 1;
 
 /*함수 선언*/
+void *reader_task(void* name);
+void *writer_upper_task(void* name);
+void *writer_lower_task(void* name);
 void upper();
 void lower();
-void *reader_task();
-void *writer_upper_task();
-void *writer_lower_task();
-void *data_logger(void task(), char* name);
 
 int main()
 {
@@ -35,24 +34,23 @@ int main()
     for(int i = 0; i<5; i++)
     {
         sprintf(readerName[i], "%s%d", "reader", i+1);
-        pthread_setname_np(reader[i], readerName[i]);
     }
 
     // writer의 이름 배열 선언 및 초기화
     char* writerName[2] = {"writer_upper", "writer_lower"};
 
     /*thread create*/
-    pthread_create(&reader[0],NULL,data_logger,(reader_task(), readerName[0]));
-    pthread_create(&reader[1],NULL,data_logger,(reader_task(),readerName[1]));
+    pthread_create(&reader[0],NULL,reader_task,(void*)readerName[0]);
+    pthread_create(&reader[1],NULL,reader_task,(void*)readerName[1]);
 
-    pthread_create(&writer_upper,NULL,data_logger,(writer_upper_task(), writerName[0]);  // 데이터를 대문자로 바꾸는 writer => HAPPY MERRY CHRISTMAS~!
+    pthread_create(&writer_upper,NULL,writer_upper_task,(void*)writerName[0]);  // 데이터를 대문자로 바꾸는 writer => HAPPY MERRY CHRISTMAS~!
 
-    pthread_create(&reader[2],NULL,data_logger,(reader_task(),readerName[2]));
-    pthread_create(&reader[3],NULL,data_logger,(reader_task(),readerName[3]));
+    pthread_create(&reader[2],NULL,reader_task,(void*)readerName[2]);
+    pthread_create(&reader[3],NULL,reader_task,(void*)readerName[3]);
 
-    pthread_create(&writer_lower,NULL,data_logger,(writer_upper_task(), writerName[1]));    // 데이터를 소문자로 바꾸는 writer => happy merry christmas~!
+    pthread_create(&writer_lower,NULL,writer_lower_task,(void*)writerName[1]);    // 데이터를 소문자로 바꾸는 writer => happy merry christmas~!
 
-    pthread_create(&reader[4],NULL,data_logger,(reader_task(),readerName[4]));
+    pthread_create(&reader[4],NULL,reader_task,(void*)readerName[4]);
 
 
     for(int i = 0; i<5; i++)
@@ -64,48 +62,7 @@ int main()
     return 0;
 }
 
-void *reader_task()
-{
-    printf("%s", S);
-}
-
-void *writer_upper_task()
-{
-    int i;
-    for (i = 0; S[i] != NULL; i++) {
-        if (S[i] >= 'a' && S[i] <= 'z')
-            S[i] -= 'a' - 'A';
-    }
-}
-
-void *writer_lower_task()
-{
-    int i;
-    for (i = 0; S[i] != NULL; i++) {
-        if (S[i] >= 'A' && S[i] <= 'Z')
-            S[i] += 'a' - 'A';
-    }
-}
-
-void upper() {
-    int i;
-    for (i = 0; S[i] != NULL; i++) {
-        if (S[i] >= 'a' && S[i] <= 'z')
-            S[i] -= 'a' - 'A';
-    }
-}
-
-// 소문자로 바꿔주는 함수
-void lower() {
-    int i;
-
-    for (i = 0; S[i] != NULL; i++) {
-        if (S[i] >= 'A' && S[i] <= 'Z')
-            S[i] += 'a' - 'A';
-    }
-}
-
-void *data_logger(void task(), char* name)
+void *reader_task(void* name)
 {
     time_t currentTime;
     struct tm* timeInfo;
@@ -119,9 +76,70 @@ void *data_logger(void task(), char* name)
         time(&currentTime);
         timeInfo = localtime(&currentTime);
         strftime(currentTimeString, 128, "%Y-%m-%d %H:%M:%S", timeInfo);
-        task(name);
+        printf("%s", S);
         fprintf(file, "%s\t%s\t%s\t%d\n", currentTimeString, (char*)name, S, count);
 
         count++;
+    }
+}
+
+void *writer_upper_task(void* name)
+{
+        time_t currentTime;
+    struct tm* timeInfo;
+    char currentTimeString[128];
+
+    FILE* file;
+    int i = 0;
+        file = fopen("event.log", "a");
+
+    for (i = 0; i < 100; i++) {
+        time(&currentTime);
+        timeInfo = localtime(&currentTime);
+        strftime(currentTimeString, 128, "%Y-%m-%d %H:%M:%S", timeInfo);
+        upper();
+        fprintf(file, "%s\t%s\t%s\t%d\n", currentTimeString, (char*)name, S, count);
+
+        count++;
+    }
+}
+
+void *writer_lower_task(void* name)
+{
+    time_t currentTime;
+    struct tm* timeInfo;
+    char currentTimeString[128];
+
+    FILE* file;
+    int i = 0;
+        file = fopen("event.log", "a");
+
+    for (i = 0; i < 100; i++) {
+        time(&currentTime);
+        timeInfo = localtime(&currentTime);
+        strftime(currentTimeString, 128, "%Y-%m-%d %H:%M:%S", timeInfo);
+        lower();
+        fprintf(file, "%s\t%s\t%s\t%d\n", currentTimeString, (char*)name, S, count);
+
+        count++;
+    }
+}
+
+//문자열을 대문자로 바꾸는 함수
+void upper() {
+    int i;
+    for (i = 0; S[i] != NULL; i++) {
+        if (S[i] >= 'a' && S[i] <= 'z')
+            S[i] -= 'a' - 'A';
+    }
+}
+
+//문자열을 소문자로 바꾸는 함수
+void lower() {
+    int i;
+
+    for (i = 0; S[i] != NULL; i++) {
+        if (S[i] >= 'A' && S[i] <= 'Z')
+            S[i] += 'a' - 'A';
     }
 }
