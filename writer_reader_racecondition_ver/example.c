94b7592f18ed9ab91dd26b4cfd17f4b0d96d3787
Huejii <94972402+Huejii@users.noreply.github.com>
2022-11-19 21:51:23 +0900

    Update example.c

--- writer_reader_racecondition_ver/example.c
+++ writer_reader_racecondition_ver/example.c
@@ -8,7 +8,7 @@ int main()
 {
     for (int i = 0; i< strlen(S); i++) {
         if (S[i] >= 'a' && S[i] <= 'z')
-            S[i] = toupper(S[i]);
+            S[i] -= 32 ;
     }
     printf("%s\n", S);
     return 0;
