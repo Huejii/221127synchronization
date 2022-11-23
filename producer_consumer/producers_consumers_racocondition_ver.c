aa47913193971ceb794bcc3377081e3dfb50c2e9
Huejii <94972402+Huejii@users.noreply.github.com>
2022-11-23 20:53:24 +0900

    Update producers_consumers_racocondition_ver.c

--- producer_consumer/producers_consumers_racocondition_ver.c
+++ producer_consumer/producers_consumers_racocondition_ver.c
@@ -48,17 +48,9 @@ int main()
     int random = 0; // 정수형 변수 선언
     char winner;
     int i;
-
-	for (int i = 0; i < 10; i++) { // 10번 반복
-
-		random = rand()%2; // 난수 생성
-
-		printf("%d\n", random); // 출력
-    }
-
-
+    
+    circular_init();
     printf("// 버퍼게임 시작\n");
-
 
 
     /*thread create*/
