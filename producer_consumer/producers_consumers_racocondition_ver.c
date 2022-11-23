d61dafbfec2f6949af730deed14b04b5f48c2788
Huejii <94972402+Huejii@users.noreply.github.com>
2022-11-23 22:24:52 +0900

    Update producers_consumers_racocondition_ver.c

--- producer_consumer/producers_consumers_racocondition_ver.c
+++ producer_consumer/producers_consumers_racocondition_ver.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
-#define MAX_CIRCULAR_SIZE 20000             // 링버퍼가 갖는 아이템 개수
+#define MAX_CIRCULAR_SIZE 2000             // 링버퍼가 갖는 아이템 개수
 #define TEAM_THREAD_SIZE 5
 
 /*
@@ -55,10 +55,9 @@ int main()
     srand(time(NULL));
 
     /*thread create*/
-    for(i = 0; i <20000; i++)
+    for(i = 0; i <2000; i++)
     {
         random = rand(); // 난수 생성
-        printf("%d\n",random);
         random %= 2; // 난수 생성
         switch (random){
             case 0:
@@ -76,20 +75,20 @@ int main()
 
         if(teamA_buffer->tail == teamA_buffer->head && teamB_buffer->tail == teamB_buffer->head)
         {
-            printf("i ==20000, C\n");
-            i=20000; // 게임 종료
+            printf("i ==2000, C\n");
+            i=2000; // 게임 종료
             winner = 'C';
         }
         else if(teamA_buffer->tail == teamA_buffer->head)
         {
-            printf("i ==20000, A\n");
+            printf("i ==2000, A\n");
             i=20000; // 게임 종료
             winner = 'A';
         }
         else if(teamB_buffer->tail == teamB_buffer->head)
         {   
-            printf("i ==20000, B\n");
-            i=20000; // 게임 종료
+            printf("i ==2000, B\n");
+            i=2000; // 게임 종료
             winner = 'B';
         }
     }
@@ -117,7 +116,7 @@ void circular_init()
     // teamB_buffer->head=0;
     // teamB_buffer->tail = 0; // 태그 값을 0으로 초기화
     
-    for(int i =0; i < 10000; i++)
+    for(int i =0; i < 1000; i++)
     {
         // buffer에 데이터 저장
         teamA_buffer->item[teamA_buffer->head] = i+1;
@@ -130,7 +129,7 @@ void circular_init()
 
     }
 
-    for(int i =0; i < 10000; i++)
+    for(int i =0; i < 1000; i++)
     {
         teamB_buffer->item[teamB_buffer->head] = i+1001;
 
